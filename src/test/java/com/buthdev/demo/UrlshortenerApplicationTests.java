77e5a046799b242cb189540d5898c800ebdc1cdf
Matheus <matheusdsouzabuth@gmail.com>
2025-07-01 09:19:38 -0300

    Initial commit

---
+++ src/test/java/com/buthdev/demo/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buthdev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
